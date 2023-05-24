1e862a05ee84eacd8c76dfeb850ef32795f6dc5b
Isabela Souza <isabela-karolina@hotmail.com>
2023-05-24 20:50:20 -0300

    Tornando o código mais legível.

--- Programa.java
+++ Programa.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 class Programa {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    boolean erroTentativas = false;
-    Pessoa pessoa = new Pessoa();
-    Mensagem mensagem = new Mensagem();
-    Validacao validacao = new Validacao();
 
     System.out.println("\tRelatório Auxiliar de Controle de Infecções");
 
